e5740b784000492fae08349b4d4bfbe8df0c25a7
KirillTolkachev <k-tolkachev20031203@mail.ru>
2019-04-05 02:32:24 +0300

    almost

--- HW_03.04.2019/b.ml
+++ HW_03.04.2019/b.ml
@@ -1,1 +1,16 @@
-print_string (String.make 100 'a');;
+let rec print1 n = 
+ for i = 0 to n do 
+    print_string ( String.make i ' ' ); print_string "*\n"
+  done ;;
+
+let rec print2 n = 
+  if n > 0 then 
+    (print_string (String.make n ' ') ; print_string "*") 
+  else if n <= 0 then 
+    print_string "*" 
+  else failwith "";;
+     
+  let rec print n = 
+  print1 (n / 2); print2 (n / 2) ;;
+
+  print 20;;
